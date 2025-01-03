d658c8468ee525e1526d76f276150cec6caf4c84
Rostyslav Onikiienko <ronikiienko@gmail.com>
2025-01-02 17:08:29 -0800

    refactor perf monitor to be more flexible

--- src/perf_monitor.rs
+++ src/perf_monitor.rs
@@ -5,7 +5,7 @@ pub struct PerfMonitor {
     clocks: HashMap<String, Instant>,
     sample_size: i32,
     current_sample: i32,
-    summary: String
+    ms_per_frames: HashMap<String, f32>
 }
 
 impl PerfMonitor {
@@ -14,19 +14,19 @@ impl PerfMonitor {
             clocks: HashMap::new(),
             sample_size: 60,
             current_sample: 0,
-            summary: String::new()
+            ms_per_frames: HashMap::new()
         }
     }
 
     pub fn start_frame(&mut self) -> bool {
         if self.current_sample >= self.sample_size {
-            self.summary = String::new();
-            for (label, instant) in self.clocks.iter_mut() {
+            let ms_per_frame: HashMap<_, _> = self.clocks.iter().map(|(label, instant)| {
                 let elapsed = instant.elapsed();
-                let duration_per_frame = ((elapsed.as_micros() as f64) / 1000.0) / self.sample_size as f64;
-                self.summary.push_str(&format!("{}: {}fps\n", label, 1000.0 / duration_per_frame));
-                *instant = Instant::now();
-            }
+                let ms_per_frame = ((elapsed.as_micros() as f64) / 1000.0) / self.sample_size as f64;
+                (label.clone(), ms_per_frame as f32)
+            }).collect();
+            self.clocks.iter_mut().for_each(|(_, instant)| *instant = Instant::now());
+            self.ms_per_frames = ms_per_frame;
             self.current_sample = 0;
             true
         } else {
@@ -43,7 +43,13 @@ impl PerfMonitor {
         self.clocks.remove(label);
     }
 
-    pub fn get_summary(&self) -> &str {
-        &self.summary
+    pub fn get_summary(&self) -> String {
+        self.ms_per_frames.iter().fold("".to_string(), |acc, (label, ms_per_frame)| {
+            format!("{}: {:.1} fps\n", &label, 1000.0 / ms_per_frame)
+        })
     }
+
+    // pub fn get_fps(&self, label: &str) -> Option<f32> {
+    //
+    // }
 }
